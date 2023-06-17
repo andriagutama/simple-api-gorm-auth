1647ce7b8e3156fd011d34f35dd07d68b11f4e75
Andria Gutama <andriagutama@gmail.com>
2023-06-17 11:36:46 +0700

    api/index.go add env

--- api/index.go
+++ api/index.go
@@ -8,5 +8,5 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-	fmt.Println(os.Getenv("POSTGRES_URL"))
+	fmt.Println("POSTGRES_URL :", os.Getenv("POSTGRES_URL"))
 }
