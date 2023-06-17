079d017d3a6eab9e9bfc81d081ef41a528ace892
Andria Gutama <andriagutama@gmail.com>
2023-06-17 12:49:35 +0700

    POSTGRES_URL

--- api/index.go
+++ api/index.go
@@ -3,10 +3,10 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
-	fmt.Println("Postgresql :", os.Getenv("POSTGRES_URL"))
+	//fmt.Println("Postgresql :", os.Getenv("POSTGRES_URL"))
+	fmt.Println("Postgresql")
 }
