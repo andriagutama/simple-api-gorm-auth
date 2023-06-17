696a942b1cbc493a799c969653fb45931987b56c
Andria Gutama <andriagutama@gmail.com>
2023-06-17 12:37:12 +0700

    api/index.go

--- api/index.go
+++ api/index.go
@@ -2,11 +2,18 @@ package api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv" // go get -u github.com/joho/godotenv
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	errEnv := godotenv.Load(".env")
+	if errEnv != nil {
+		log.Fatal("Error env")
+	}
 	fmt.Println("POSTGRES_URL :", os.Getenv("POSTGRES_URL"))
 }
