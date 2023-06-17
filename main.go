10d77fda219268d5c8a5d83b7df6a99a6cbe9481
Andria Gutama <andriagutama@gmail.com>
2023-06-17 09:13:52 +0700

    main.go

--- main.go
+++ main.go
@@ -20,40 +20,8 @@ import (
 	"github.com/joho/godotenv" // go get -u github.com/joho/godotenv
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "just4andria"
-	dbname   = "db_reff"
-)
-
 func postHandler(c *gin.Context, db *gorm.DB) {
 	var student models.Student
-
-	/*if c.Bind(&student) == nil {
-		_, err := db.Exec("insert into students (student_name, student_age, student_address, student_phone_no) values ($1, $2, $3, $4)",
-			student.Student_name,
-			student.Student_age,
-			student.Student_address,
-			student.Student_phone_no,
-		)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "success created",
-		})
-		return
-	}
-
-	c.JSON(http.StatusBadRequest, gin.H{
-		"message": "error",
-	})*/
 
 	c.Bind(&student)
 	db.Create(&student)
@@ -63,52 +31,8 @@ func postHandler(c *gin.Context, db *gorm.DB) {
 	})
 }
 
-/*func rowToStruct(rows *sql.Rows, dest interface{}) error {
-	destv := reflect.ValueOf(dest).Elem()
-	args := make([]interface{}, destv.Type().Elem().NumField())
-
-	for rows.Next() {
-		rowp := reflect.New(destv.Type().Elem())
-		rowv := rowp.Elem()
-
-		for i := 0; i < rowv.NumField(); i++ {
-			args[i] = rowv.Field(i).Addr().Interface()
-		}
-
-		if err := rows.Scan(args...); err != nil {
-			return err
-		}
-
-		destv.Set(reflect.Append(destv, rowv))
-	}
-
-	return nil
-}*/
-
 func getAllHandler(c *gin.Context, db *gorm.DB) {
 	var student []models.Student
-
-	/*row, err := db.Query("SELECT * FROM students ORDER BY student_id ASC")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	rowToStruct(row, &student)
-
-	if student == nil {
-		c.JSON(http.StatusNotFound, gin.H{
-			"message": "data not found",
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "ok",
-		"data":    student,
-	})*/
 
 	db.Find(&student)
 	c.JSON(http.StatusOK, gin.H{
@@ -121,29 +45,6 @@ func getHandler(c *gin.Context, db *gorm.DB) {
 	var student models.Student
 
 	studentId := c.Param("student_id")
-
-	/*row, err := db.Query("SELECT * FROM students WHERE student_id = $1",
-		studentId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	rowToStruct(row, &student)
-
-	if student == nil {
-		c.JSON(http.StatusNotFound, gin.H{
-			"message": "data not found",
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "ok",
-		"data":    student,
-	})*/
 
 	id, _ := strconv.ParseUint(studentId, 10, 64)
 	if db.Find(&student, "student_id=?", id).RecordNotFound() {
@@ -163,26 +64,6 @@ func putHandler(c *gin.Context, db *gorm.DB) {
 	var student models.Student
 
 	studentId := c.Param("student_id")
-
-	/*if c.Bind(&student) == nil {
-		_, err := db.Exec("update students set student_name=$1 where student_id=$2",
-			student.Student_name, studentId)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "success update",
-		})
-		return
-	}
-
-	c.JSON(http.StatusBadRequest, gin.H{
-		"message": "error",
-	})*/
 
 	id, _ := strconv.ParseUint(studentId, 10, 64)
 	if db.Find(&student, "student_id=?", id).RecordNotFound() {
@@ -205,19 +86,6 @@ func putHandler(c *gin.Context, db *gorm.DB) {
 func deleteHandler(c *gin.Context, db *gorm.DB) {
 	studentId := c.Param("student_id")
 
-	/*_, err := db.Exec("delete from students where student_id = $1",
-		studentId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "success delete",
-	})*/
-
 	var student models.Student
 	id, _ := strconv.ParseUint(studentId, 10, 64)
 	if db.Find(&student, "student_id=?", id).RecordNotFound() {
@@ -234,10 +102,6 @@ func deleteHandler(c *gin.Context, db *gorm.DB) {
 }
 
 func setRouter() *gin.Engine {
-	//conn := "postgresql://postgres:just4andria@127.0.0.1/db_tutorial?sslmode=disable"
-	//conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-	//	host, port, user, password, dbname)
-
 	errEnv := godotenv.Load(".env")
 	if errEnv != nil {
 		log.Fatal("Error env")
